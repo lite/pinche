621788166c304f0c9ec3bf27fe25921d7b251a9a
lite <litekok@gmail.com>
2013-05-24 23:25:08 +0800

    add emberjs for html5

--- pinche-robot/rules/news.js
+++ pinche-robot/rules/news.js
@@ -12,7 +12,7 @@ module.exports = {
           url: host + 'pinche/1/'
         },
         {
-          title: '图文消息描述3图文消息描述3图文消息描述3图文消息描', 
+          title: '新闻2', 
           description: '图文消息描述2', 
           pic: '', 
           url: host + 'pinche/2/'
@@ -22,6 +22,48 @@ module.exports = {
           description: '图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3图文消息描述3', 
           pic: host + '', 
           url: host + 'pinche/3/'
+        }
+        {
+          title: '新闻4', 
+          description: '图文消息描述4', 
+          pic: host + '', 
+          url: host + 'pinche/4/'
+        }
+        {
+          title: '新闻5', 
+          description: '图文消息描述4', 
+          pic: host + '', 
+          url: host + 'pinche/4/'
+        }
+        {
+          title: '新闻6', 
+          description: '图文消息描述4', 
+          pic: host + '', 
+          url: host + 'pinche/4/'
+        }
+        {
+          title: '新闻7', 
+          description: '图文消息描述4', 
+          pic: host + '', 
+          url: host + 'pinche/4/'
+        }
+        {
+          title: '新闻8', 
+          description: '图文消息描述4', 
+          pic: host + '', 
+          url: host + 'pinche/4/'
+        }
+        {
+          title: '新闻9', 
+          description: '图文消息描述4', 
+          pic: host + '', 
+          url: host + 'pinche/4/'
+        }
+        {
+          title: '新闻10', 
+          description: '图文消息描述4', 
+          pic: host + '', 
+          url: host + 'pinche/4/'
         }
       ];
       // 发送 "news 1" 时只回复一条图文消息
