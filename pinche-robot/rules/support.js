e2c3ea101f91125b175f8970991d57023006ea28
lite <litekok@gmail.com>
2013-06-27 01:11:04 +0800

    信息在标题

--- pinche-robot/rules/support.js
+++ pinche-robot/rules/support.js
@@ -60,8 +60,7 @@ function parse_result(param, data){
     data = pointList[i];
     var distance = distanceByLnglat(data.location.lng, data.location.lat, param.lng, param.lat);
     var item = {}
-    item.title = data.name;
-    item.description = "地址：" + data.address + "\n距离：" + distance + "米";
+    item.title = data.name + "地址：" + data.address + "\n距离：" + distance + "米";
     item.pic = 'http://mmsns.qpic.cn/mmsns/1QPBGwF70BeO3RVsMHK3YepkI2zcticXibKibYic0a49fxNHJD8zLa8S0A/0';
     item.url = 'http://m.autohome.com.cn/brand/33/?search=1';
     params.push(item);
